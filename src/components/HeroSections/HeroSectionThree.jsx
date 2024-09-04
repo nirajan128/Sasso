bd838fe231782afd9eecb761bd37f41cac8928be
Nirajan <nirajanshrestha672@gmail.com>
2024-09-04 16:05:37 -0400

    fixed font sizes

--- src/components/HeroSections/HeroSectionThree.jsx
+++ src/components/HeroSections/HeroSectionThree.jsx
@@ -3,7 +3,9 @@ function HeroSectionThree () {
   return (
     <div className='container d-flex flex-column justify-content-center mt-3'>
       <div className='d-flex flex-row'>
-        <h4 className='raleway text-light customBg p-3'>What we Create?</h4>
+        <p className='raleway text-light customBg p-3 customSize'>
+          WHAT WE CREATE ?
+        </p>
         <div className='social-icons2 d-flex align-items-center'>
           <a href='https://www.instagram.com' target='_blank'>
             <i className='fab fa-instagram'></i>
