a2b4d1c7e0af1bf4f79e5ff9ec05d50e0e23037f
Nirajan <nirajanshrestha672@gmail.com>
2024-09-05 18:58:38 -0400

    created data.js and added menu data

---
+++ src/data.js
@@ -0,0 +1,92 @@
+const data = [
+  {
+    title: 'Starters',
+    menu: [
+      {
+        name: 'Soup',
+        price: '$9',
+        status: '',
+        desc: 'Daily chefs creation, see serve'
+      },
+      {
+        name: 'Ceaser',
+        price: '$13.8/18.8',
+        status: 'VE',
+        desc: 'Romaine, fried capers, sourdough croutons, Grana Padano, tossed in house dressing'
+      },
+      {
+        name: 'The rocket',
+        price: '$14.3/19.3',
+        status: 'GF,VE',
+        desc: 'Arugula, Grana Padano, cured egg yolk, dressed in fresh lemon vinaigrette'
+      },
+      {
+        name: 'Orchard Salad',
+        price: '$14.5/19.8',
+        status: 'GF,DF,VE',
+        desc: 'Mixed greens, roasted apples, seeds, nuts, honey, dressed in an apple vinaigrette'
+      },
+      {
+        name: 'Burrata on Toast',
+        price: '$23',
+        status: 'VE',
+        desc: 'Grilled sourdough, roast garlic spread, tomatoes, arugula, basil olive oil'
+      }
+    ]
+  },
+  {
+    title: 'Pasta',
+    menu: [
+      {
+        name: 'Substitute for Gluten free pasta',
+        price: '$3',
+        status: '',
+        desc: ''
+      },
+      {
+        name: 'Marky Mark',
+        price: '$24',
+        status: 'DF',
+        desc: 'House made sausage, pepperoncini and tomato sauce with cavatelli'
+      },
+      {
+        name: 'Sea Dis',
+        price: '$25.2',
+        status: '',
+        desc: 'Shrimp and mussels in a garlic-tarragon cream sauce over linguine'
+      },
+      {
+        name: 'Parmigiano',
+        price: '$28',
+        status: '',
+        desc: 'Breaded chicken breast, tomato sauce and provolone over linguine'
+      },
+      {
+        name: 'Putta',
+        price: '$21',
+        status: 'VE',
+        desc: 'Red onion, roasted red peppers, olives, sundried tomatoes, capers, tomatoes, eggplant, over penne in a spicy tomato stew Add anchovy | $3.3'
+      },
+      {
+        name: 'Chicken Rose',
+        price: '$24',
+        status: '',
+        desc: 'Roasted chicken, sundried tomatoes, spinach, goat cheese over penne in a creamy tomato sauce'
+      },
+      {
+        name: 'Truffel Cavateli',
+        price: '$24.2',
+        status: 'VE',
+        desc: 'Roast mushrooms and garlic tossed with cavatelli pasta in a white truffle cream sauce, topped with parm breadcrumb'
+      }
+    ]
+  },
+  { title: 'Sharing' },
+  { title: ' As Mains' },
+  { title: 'Sammys' },
+  { title: 'Pizza Pomodori' },
+  { title: 'Make it your own - $14.4' },
+  { title: 'Pizza Non pomodori' }
+]
+
+export default data
