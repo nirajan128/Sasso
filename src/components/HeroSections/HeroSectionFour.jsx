5b2980b52cedd4859b47220ba8c59515488d9e03
Nirajan <nirajanshrestha672@gmail.com>
2024-09-04 23:38:43 -0400

    addded credits section, redsign connectwithus

--- src/components/HeroSections/HeroSectionFour.jsx
+++ src/components/HeroSections/HeroSectionFour.jsx
@@ -6,17 +6,45 @@ function HeroSectionFour () {
         <div className='col-sm-12 col-lg-8  d-flex justify-content-center customShadow'>
           <img src={f11} alt='' className='img-fluid' />
         </div>
-        <div className='col-sm-12 col-lg-4 open-sans'>
-          <div className='d-flex justify-content-center flex-column  h-100'>
-            <h4>Join Us</h4>
+        <div className='col-sm-12 col-lg-4 raleway'>
+          <div className='d-flex justify-content-center flex-column  h-100 p-3'>
+            <h4>CONNECT WITH US</h4>
+            <p>
+              <span>
+                <i
+                  className='fa-solid fa-phone'
+                  style={{ color: '#800000' }}
+                ></i>
+              </span>
+              905 - 526 - 4848
+            </p>
+            <p>
+              <span>
+                <i
+                  className='fa-solid fa-envelope'
+                  style={{ color: '#800000' }}
+                ></i>
+              </span>
+              <a href='mailto:sassopizzasfoh@gmail.com'>
+                sassopizzasfoh@gmail.com
+              </a>
+            </p>
+
             <button type='button' className='btn btn-dark mt-1'>
-              TEAM
+              JOIN OUR TEAM
             </button>
             <button type='button' className='btn  btn-dark mt-1'>
-              NEWSLETTER
+              JOIN OUR NEWSLETTER
             </button>
           </div>
         </div>
+      </div>
+      <div className='row customBg d-flex  justify-content-center align-items-center raleway'>
+        <p className='credits text-light p-2 text-center'>
+          {' '}
+          Designed and Developed by{' '}
+          <a href='https://github.com/nirajan128'>@NIRAJAN SHRSETHA</a>
+        </p>
       </div>
     </div>
   )
